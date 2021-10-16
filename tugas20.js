b3def61de3942443256afa0a84a6931ba0915087
Ardi Haryono <ardiharyono26@gmail.com>
2021-10-16 17:35:19 +0700

    Tugas 20 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas20.js
@@ -0,0 +1,8 @@
+function panggilAku() {
+  var cari = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+  console.log(cari.match("are"));
+  console.log("null");
+  console.log(cari.match("words"));
+}
+
+panggilAku()
